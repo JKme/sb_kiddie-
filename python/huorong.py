95c1567fe32b9919c55dc9aef625e1bfdaf1929a
cocokey <2935865+JKme@users.noreply.github.com>
2021-08-09 15:05:10 +0800

    Update huorong.py

--- python/huorong.py
+++ python/huorong.py
@@ -36,37 +36,61 @@ from selenium.webdriver import Chrome
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
+import requests
+import json
 import time
 
 
 def spider():
-    # option = webdriver.ChromeOptions()
-    # option.add_experimental_option("--headless")
-    # option.add_experimental_option("detach", True)
-
+    old_num, page = read_log()
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
 
     web = Chrome(options=chrome_options)
 
-    web.get('https://ngabbs.com/read.php?tid=27586426&authorid=61078637&page=3')
+    web.get(f'https://ngabbs.com/read.php?tid=27586426&authorid=61078637&page={page}')
 
     time.sleep(3)  # 睡眠2秒
     html = web.find_element_by_xpath("//*").get_attribute("outerHTML")
-    print(html)
-    getContents(html)
-    # web.find_element_by_xpath('//*[@id="pagebtop"]/table/tbody/tr/td[4]/a').click()
 
-
-def getContents(html):
     pattern = re.compile(r"<span id=\"postcontent(\d+)\" class=\"postcontent ubbcode\">(.*?)<img.*?src=\"(.*?)\"")
     match = re.findall(pattern, html)
-    if match:
-        print(match)
+    lastContent = match[-1:][0]
+    msg_num = lastContent[0]
+    msg_txt = lastContent[1]
+    record_log(msg_num, page)
+    print(msg_txt, msg_num)
+    # robot("Success " + msg_txt)
+    if msg_num > old_num:
+        robot("Success " + msg_txt)
+
+    if len(match) == 20:
+        record_log(msg_num, int(page)+1)
+
+
+def record_log(msg_num, page):
+    with open("/var/tmp/stock.txt", "w+") as f:
+        f.write(f"{msg_num}|{page}")
+
+
+def read_log():
+    with open("/var/tmp/stock.txt", "r") as f:
+        data = f.readline().strip("\n").split("|")
+        return data[0], data[1]
+
+
+def robot(msg):
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    msg = msg.replace("<br>", "\n")
+    url = 'https://hooks.slack.com/services/<token>'
+    data = {"channel": "#general", "username": "GAN", "text": msg, "type": "mrkdwn"}
+    requests.post(url, data={"payload": json.dumps(data)}, headers=headers)
 
 
 if __name__ == "__main__":
     spider()
-
     
     
